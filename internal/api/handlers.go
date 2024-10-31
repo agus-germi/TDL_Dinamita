6c79885fcc70051a118bb5fe134abe9f24feee4a
Sebakrag <sebaskrag@gmail.com>
2024-10-30 21:47:32 -0300

    Fixing response messages of RegisterUser request.

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,13 +1,16 @@
 package api
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/agus-germi/TDL_Dinamita/internal/api/dtos"
 	"github.com/agus-germi/TDL_Dinamita/internal/service"
 	"github.com/labstack/echo/v4"
 )
+
+type responseMessage struct {
+	Message string `json:"message"`
+}
 
 func (a *API) RegisterUser(c echo.Context) error {
 	ctx := c.Request().Context()
@@ -16,21 +19,21 @@ func (a *API) RegisterUser(c echo.Context) error {
 
 	err := c.Bind(&params)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+		return c.JSON(http.StatusBadRequest, responseMessage{Message: "Invalid request"}) // otra opcion--> Message: err.Error()
 	}
 
 	err = a.dataValidator.Struct(params)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
+		return c.JSON(http.StatusBadRequest, responseMessage{Message: err.Error()})
 	}
 
 	err = a.serv.RegisterUser(ctx, params.Name, params.Password, params.Email)
 	if err != nil {
 		if err == service.ErrUserAlreadyExists {
-			return c.JSON(http.StatusConflict, err.Error())
+			return c.JSON(http.StatusConflict, responseMessage{Message: err.Error()})
 		}
-		return c.JSON(http.StatusInternalServerError, errors.New("unexpected error"))
+		return c.JSON(http.StatusInternalServerError, responseMessage{Message: "Internal server error"})
 	}
 
-	return c.JSON(http.StatusCreated, nil)
+	return c.JSON(http.StatusCreated, responseMessage{Message: "User registered successfully"})
 }
