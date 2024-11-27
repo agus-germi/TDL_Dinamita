963aa3cd87990a879042282f340856a6ad9632f3
agusgermi <agerminario@fi.uba.ar>
2024-11-27 14:01:14 -0300

    email sending for reservation confirmation working

--- internal/service/notification/email.go
+++ internal/service/notification/email.go
@@ -3,16 +3,8 @@ package notification
 import (
 	"log"
 
-	"github.com/joho/godotenv"
 	"gopkg.in/gomail.v2"
 )
-
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-}
 
 func SendReservationConfirmationEmail(to string, reservationDetails string) error {
 	mailer := gomail.NewMessage()
@@ -20,12 +12,6 @@ func SendReservationConfirmationEmail(to string, reservationDetails string) erro
 	mailer.SetHeader("To", to)
 	mailer.SetHeader("Subject", "Reservation Confirmation")
 	mailer.SetBody("text/html", reservationDetails)
-
-	/*emailPassword := os.Getenv("EMAIL_PASSWORD") //TODO: agregar contraseñas al .env y luego tiene que ir en el lugar de la password en dialer
-	if emailPassword == "" {
-		log.Printf("Email password not set in environment variables")
-		return fmt.Errorf("email password not set")
-	}*/
 
 	dialer := gomail.NewDialer("smtp.gmail.com", 587, "agmgerminario@gmail.com", "iwos arnt ngur yfte")
 
