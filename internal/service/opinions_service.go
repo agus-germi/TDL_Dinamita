933a69835c8db100f4015060cf8ec2042a3302c4
Sebakrag <sebaskrag@gmail.com>
2024-12-09 03:50:18 -0300

    Adding duration limitation to Opinion service operations.

--- internal/service/opinions_service.go
+++ internal/service/opinions_service.go
@@ -1,48 +1,55 @@
 package service
 
 import (
-    "context"
-    "errors"
-    "log"
-    
-    "github.com/agus-germi/TDL_Dinamita/internal/models"
+	"context"
+	"errors"
+
+	"github.com/agus-germi/TDL_Dinamita/internal/entity"
+	"github.com/agus-germi/TDL_Dinamita/internal/models"
 )
 
 var (
-    ErrOpinionNotFound = errors.New("opinion not found")
-    ErrAddingOpinion   = errors.New("something went wrong trying to add an opinion")
+	ErrOpinionNotFound = errors.New("opinion not found")
+	ErrAddingOpinion   = errors.New("something went wrong trying to add an opinion")
 )
 
 func (s *serv) CreateOpinion(ctx context.Context, userID int64, opinionText string, opinionRating int) error {
-    log.Printf("Creating opinion for user_id: %d with opinion: %s and rating: %d", userID, opinionText, opinionRating)
-    
-    err := s.repo.SaveOpinion(ctx, userID, opinionText, opinionRating)
-    if err != nil {
-        return ErrAddingOpinion
-    }
-    
-    return nil
+	s.log.Debugf("Creating opinion for user_id: %d with opinion: %s and rating: %d", userID, opinionText, opinionRating)
+
+	err := s.executeWithTimeout(ctx, s.config.MaxDBOperationDuration, func(ctx context.Context) error {
+		return s.repo.SaveOpinion(ctx, userID, opinionText, opinionRating)
+	})
+
+	if err != nil {
+		return ErrAddingOpinion
+	}
+
+	return nil
 }
 
 func (s *serv) GetOpinions(ctx context.Context) (*[]models.Opinion, error) {
-    opinions, err := s.repo.GetAllOpinions(ctx)
-    if err != nil {
-        return nil, err
-    }
+	var opinions *[]entity.Opinion
+	err := s.executeWithTimeout(ctx, s.config.MaxDBOperationDuration, func(ctx context.Context) error {
+		var err error
+		opinions, err = s.repo.GetAllOpinions(ctx)
+		return err
+	})
 
-    log.Printf("Retrieved opinions: %+v", opinions)
-    modelOpinions := make([]models.Opinion, len(*opinions))
-    for i, opinion := range *opinions {
-        modelOpinions[i] = models.Opinion{
-            ID:      opinion.ID,
-            Name:    opinion.Name,
-            UserID:  opinion.UserID,
-            Opinion: opinion.Opinion,
-            Rating:  opinion.Rating, 
+	if err != nil {
+		return nil, err
+	}
 
-        }
-    }
+	s.log.Debugf("Retrieved opinions: %+v", opinions)
+	modelOpinions := make([]models.Opinion, len(*opinions))
+	for i, opinion := range *opinions {
+		modelOpinions[i] = models.Opinion{
+			ID:      opinion.ID,
+			Name:    opinion.Name,
+			UserID:  opinion.UserID,
+			Opinion: opinion.Opinion,
+			Rating:  opinion.Rating,
+		}
+	}
 
-    return &modelOpinions, nil
+	return &modelOpinions, nil
 }
-
